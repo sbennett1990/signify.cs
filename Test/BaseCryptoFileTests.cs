49deb8e02d01ceee32175869c44275f15fc555ba
Scott Bennett <scottb@fastmail.com>
2017-09-25 13:16:01 -0400

    Add some unit tests

---
+++ Test/BaseCryptoFileTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace SignifyCS.Test {
+	public class BaseCryptoFileTests {
+		/* TODO:
+		 * - test null comment
+		 * - test a comment that exceeds comment length
+		 * - test a comment with bad header
+		 */
+
+		[Test]
+		public void CheckComment_GoodInput_ReturnsExpected() {
+			const string test_comment = "test comment";
+
+			string res = BaseCryptoFile.CheckComment(makeCommentLine(test_comment));
+
+			Assert.That(res, Is.EqualTo(test_comment));
+		}
+
+		[Test]
+		public void CheckComment_EmptyComment_ReturnsEmpty() {
+			string empty_comment = string.Empty;
+
+			string res = BaseCryptoFile.CheckComment(makeCommentLine(empty_comment));
+
+			Assert.That(res, Is.EqualTo(string.Empty));
+		}
+
+		private static string getTestComment() {
+			return makeCommentLine("test comment");
+		}
+
+		private static string makeCommentLine(string comment) {
+			return CommentHeader + comment;
+		}
+
+		private static string CommentHeader {
+			get {
+				return BaseCryptoFile.COMMENT_HEADER;
+			}
+		}
+	}
+}
---
+++ Test/SigCryptoFileTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+
+namespace SignifyCS.Test {
+	public class SigCryptoFileTests {
+		[Test]
+		public void CheckSigAlg_GoodSignature_ReturnsExpected() {
+			string test_sig = getTestSignature();
+
+			Signature res_sig = SigCryptoFile.CheckSig(test_sig);
+			string res_alg = res_sig.Algorithm;
+
+			Assert.That(res_alg, Is.EqualTo(CryptoAlg));
+		}
+
+		[Test]
+		public void CheckSigKeyNum_GoodSignature_IsCorrectLength() {
+			string test_sig = getTestSignature();
+
+			Signature res_sig = SigCryptoFile.CheckSig(test_sig);
+			byte[] res_key_num = res_sig.KeyNum;
+
+			Assert.That(res_key_num.Length, Is.EqualTo(KeyNumLen));
+		}
+
+		[Test]
+		public void CheckSigData_GoodSignature_IsCorrectLength() {
+			string test_sig = getTestSignature();
+
+			Signature res_sig = SigCryptoFile.CheckSig(test_sig);
+			byte[] res_data = res_sig.SigData;
+
+			Assert.That(res_data.Length, Is.EqualTo(SigDataLen));
+		}
+
+		private static string getTestSignature() {
+			return "RWQpoQCvhBXwzIAflNibYx/9w5bDo5NqJ69RQDjQfLeh7uQvysloRT2t6P4D3vYK0Cv/0jTpBG/lU7N/4z+RtGYGMFjE+7jirgQ=";
+		}
+
+		private static string CryptoAlg {
+			get {
+				return BaseCryptoFile.PK_ALGORITHM;
+			}
+		}
+
+		private static uint KeyNumLen {
+			get {
+				return BaseCryptoFile.KEY_NUM_LEN;
+			}
+		}
+
+		private static uint SigDataLen {
+			get {
+				return BaseCryptoFile.crypto_sign_ed25519_BYTES;
+			}
+		}
+	}
+}
